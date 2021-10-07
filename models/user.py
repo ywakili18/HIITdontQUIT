b843a49d88b943605cac15db1e1017a45d96e176
MistermindX <max.arenstein@gmail.com>
2021-10-07 12:55:44 -0400

    my_workouts work done

--- models/user.py
+++ models/user.py
@@ -6,7 +6,7 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(80), unique=True, email=True, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     first_name = db.Column(db.String(80), nullable=False)
     last_name = db.Column(db.String(80), nullable=False)
