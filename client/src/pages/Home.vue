eb6fbbc53730f8b2015a4376fd862e86d637b8d0
Brandon Hernandez <brandoncinthia0621@gmail.com>
2021-10-08 12:47:21 -0600

    almost done with homepage

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -1,6 +1,61 @@
 <template>
-  <div>
+ <div>
+   <h2>Featured workouts</h2>
+  <b-carousel
+    id="carousel-fade"
+    style="text-shadow: 0px 0px 2px #000"
+    fade
+    indicators
+    img-width="1024"
+    img-height="480"
+  >
+    <b-carousel-slide
+      caption="Push Ups"
+      
+      img-src="https://images.unsplash.com/photo-1616803689943-5601631c7fec?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80"
+    >On Odd Days
+Do 200 push-ups in as few sets as possible in addition to your regularly scheduled workout of cardio exercises. You still can do upper-body workouts on these days if you are already on a program. This is a supplemental 200 push-ups using maximum repetition sets (4 x 50, 8 x 25 ... it's your choice how you get to 200).
 
+On Even Days
+Do 200 push-ups throughout the day. This can be little sets of 10 done every half-hour or 50 push-ups done four times throughout the day. </b-carousel-slide>
+    <b-carousel-slide
+      caption="Heavy Bag"
+      img-src="https://blog.ringside.com/wp-content/uploads/2018/11/man-hitting-punching-bag.jpeg"
+    >Five rounds, 3 minutes, 1-minute breaks
+
+This effective drill will help you to develop the punch speed you need to deliver quick blows to your opponent. In boxing, they say the punch you don’t see is the one that knocks you out—and it’s true. The key is to put those punches together and fast. </b-carousel-slide>
+    <b-carousel-slide
+      caption="Pick up Basketball game"
+      img-src="https://www.globeslcc.com/wp-content/uploads/media/2016/10/cam-pickup-basketball-game-lac-cdallof-620x410.jpg"
+    >Pickup basketball games are an easy way to make new friends and meet new people.</b-carousel-slide>
+  </b-carousel>
+  <br/>
+  <br/>
+  <section>
+    <div>
+  
+  <div class="mt-4">
+    <h4>Top Workouts!</h4>
+    <b-card img-src="https://www.muscleandfitness.com/wp-content/uploads/2018/01/1109-speed-bag.jpg?w=300&quality=86&strip=all" img-left class="mb-3">
+      <b-card-text>
+        <h6>Speed Bag</h6>
+        Set your timer to 30 seconds.
+Punch at a medium pace.
+Count how many times you hit the bag in an interval.
+If the number was 40, aim to hit the bag that may times again for another 30 second round.
+Increase the number by one for eight rounds, one extra punch per round.
+      </b-card-text>
+    </b-card>
+
+    <b-card img-src="http://reemusboxing.com/wp-content/uploads/2016/07/manny-pacquiao-skipping-jump-rope-300x214.jpg" img-alt="Card image" img-right>
+      <b-card-text>
+        <h6>Jumping Rope</h6>
+        It can be used as straight cardio as well; e.g., 3 sets of 10 minutes of continuous jumping with 2 minutes of rest in between. Or 2 sets of 15 minutes of continuous jumping. Or when you get good, 30 or more minutes of jumping non-stop.
+      </b-card-text>
+    </b-card>
+  </div>
+</div>
+  </section>
 </div>
 </template>
 
