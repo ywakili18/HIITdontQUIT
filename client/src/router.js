3ddfadd1e42b4cb644f0ba23e468ba19077bf64f
Yousof Wakili <ywakili18@gmail.com>
2021-10-08 11:43:33 -0700

    basic card done

--- client/src/router.js
+++ client/src/router.js
@@ -2,11 +2,12 @@ import VueRouter from 'vue-router'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import SignUp from './components/SignUp'
-
+import About from './pages/About'
 const routes = [
   { path: '/', component: Home, name: 'Home' },
   { path: '/login', component: Login, name: 'Login' },
-  { path: '/signup', component: SignUp, name: 'SignUp' }
+  { path: '/signup', component: SignUp, name: 'SignUp' },
+  { path: '/about', component: About, name: 'About' }
 ]
 
 export default new VueRouter({ routes, mode: 'history' })
