33870dd5333c7a0d0424c69db21c4605541f22f9
MistermindX <max.arenstein@gmail.com>
2021-10-08 13:16:04 -0400

    my workouts page is on

--- client/src/pages/MyWorkouts.vue
+++ client/src/pages/MyWorkouts.vue
@@ -1,14 +1,32 @@
 <template>
   <div id="my_workouts">
-    <div class="workout_card"></div>
+    <div
+      class="workout_card"
+      v-for="workout in workouts.data"
+      :key="workout.id"
+    >
+      <p>{{ workout.duration }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
 export default {
-  name: 'My Workouts',
-  conponents: {}
+  name: 'MyWorkouts',
+  conponents: {},
+  data: () => ({
+    workouts: null
+  }),
+  mounted: async function () {
+    await this.getAllWorkouts()
+  },
+  methods: {
+    async getAllWorkouts() {
+      const res = await axios.get('http://localhost:5000/comments')
+      this.workouts = res
+    }
+  }
 }
 </script>
 
--- client/src/router.js
+++ client/src/router.js
@@ -2,11 +2,13 @@ import VueRouter from 'vue-router'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import SignUp from './components/SignUp'
+import MyWorkouts from './pages/MyWorkouts'
 
 const routes = [
   { path: '/', component: Home, name: 'Home' },
   { path: '/login', component: Login, name: 'Login' },
-  { path: '/signup', component: SignUp, name: 'SignUp' }
+  { path: '/signup', component: SignUp, name: 'SignUp' },
+  { path: '/my_workouts', component: MyWorkouts, name: 'MyWorkouts' }
 ]
 
 export default new VueRouter({ routes, mode: 'history' })
